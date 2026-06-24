206978494f84173d422ace461fbe6b3eba121cff
Deepak <104371928+Deepak2272@users.noreply.github.com>
2026-06-24 16:45:09 +0530

    initial commit

---
+++ src/main/java/com/example/jenkins/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping
+    public String sayWelcome() {
+        return "Hi!!!! Welcome";
+    }
+}
